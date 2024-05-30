6ddd1d5ba225141d74144961a15de83369cb7566
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2024-05-30 13:15:35 -0500

    :bricks: fix when it will update function the filename was unknown

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,4 @@
+set -e
 
 action=$1
 
@@ -15,16 +16,31 @@ elif [ "$action" = "update-infra" ]; then
         --capabilities CAPABILITY_NAMED_IAM \
         | cat
 
+elif [ "$action" = "delete-infra" ]; then
+    aws cloudformation delete-stack \
+        --stack-name example-lambda-app \
+        | cat
+
 elif [ "$action" = "package" ]; then
-    zip package.zip index.py
+    filename=$2
+
+    [ -z "$filename" ] && echo "second arg filename is required" && exit 1
+    [ -f "$filename" ] && echo "package '$filename' alredy exists, aborting." && exit 1
+
+    echo "creating package.zip"
+    zip "$filename" index.py
+    echo "$filename created successfully"
 
 elif [ "$action" = "deploy" ]; then
-    echo "creating 'package.zip'"
-    sh scripts.sh package
+    # get last commit hash
+    commit=$(git log --oneline -1 | awk '{print $1}')
+    filename="package_$commit.zip"
+
+    sh scripts.sh package "$filename"
 
     aws lambda update-function-code \
         --function-name lambda-example \
-        --zip-file fileb://package.zip \
+        --zip-file "fileb://$filename" \
         | cat
 else 
     echo "action '$action' not found"
