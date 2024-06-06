f3c54da73643fe2a63a0c367947e4974e9d60ccd
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2024-06-06 17:46:06 -0500

    :loud_sound: add loggin on utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,21 @@
+import logging
+
 import requests
 
-def get_pokemon_abilities(name: str):
-    res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
 
+LOG = logging.getLogger("main")
+LOG.setLevel(logging.INFO)
+
+
+def get_pokemon_abilities(name: str):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    res = requests.get(url)
+    LOG.info(f"'{url}' status_code: {res.status_code}")
     return [a.get("ability").get("name") for a in res.json().get("abilities", [])]
 
 
 def get_specie_happiness(specie: str):
-    res = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{specie}")
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{specie}"
+    res = requests.get(url)
+    LOG.info(f"'{url}' status_code: {res.status_code}")
     return res.json().get("base_happiness")
